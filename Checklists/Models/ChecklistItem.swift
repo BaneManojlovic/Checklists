77ca25d6d48730c048a2b6f2c252c9e12d8070eb
Bane Manojlovic <bane1manojlovic@gmail.com>
2019-05-26 14:14:04 +0200

    Added data model and array of model objects

---
+++ Checklists/Models/ChecklistItem.swift
@@ -0,0 +1,14 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Bane Manojlovic on 5/26/19.
+//  Copyright © 2019 Bane Manojlovic. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
