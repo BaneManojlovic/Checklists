441ae47ac177b3fdbdaf710e193d79d86617d12f
Bane Manojlovic <bane1manojlovic@gmail.com>
2019-05-26 09:53:42 +0200

    Added TableView with delegate methods

---
+++ Checklists/ViewControllers/ChecklistViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Bane Manojlovic on 5/26/19.
+//  Copyright © 2019 Bane Manojlovic. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
+// MARK: - TableView data source
+extension ChecklistViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        return cell
+    }
+}
