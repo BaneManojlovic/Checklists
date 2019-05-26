683f306cf223da9fd6e049e205a800bc676322c7
Bane Manojlovic <bane1manojlovic@gmail.com>
2019-05-26 17:34:01 +0200

    Added AddItem VC

---
+++ Checklists/ViewControllers/AddItemViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by Bane Manojlovic on 5/26/19.
+//  Copyright © 2019 Bane Manojlovic. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    // MARK: - Action methods
+    @IBAction func cancel(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func done(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+}
