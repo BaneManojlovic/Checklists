ff9e1be17b55db072e0dcee01ca042a59344e977
Branislav Manojlovic <branislav.manojlovic@zesium.com>
2019-05-28 16:18:46 +0200

    Finished chat functionality

--- Checklists/ViewControllers/Chat/ChatViewController.swift
+++ Checklists/ViewControllers/Chat/ChatViewController.swift
@@ -9,13 +9,27 @@
 import UIKit
 
 class ChatViewController: UIViewController {
+    
+    private var chatView: ChatView! {
+        loadViewIfNeeded()
+        return view as? ChatView
+    }
 
+    var viewModel: ChatViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        setup()
     }
 
     @IBAction func closeChat(_ sender: Any) {
         navigationController?.popViewController(animated: true)
+    }
+}
+
+extension ChatViewController {
+    func setup() {
+        chatView.setup()
     }
 }
